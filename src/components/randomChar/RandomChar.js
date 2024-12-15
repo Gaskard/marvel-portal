e4059ee96c2dad1de4a5b91d46b14d5971aea91c
Dmytro Shaposhnikov <dimacr137@gmail.com>
2024-12-15 22:11:54 +0200

    add CharList

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -11,7 +11,8 @@ class RandomChar extends Component {
     state = {
         char: {},
         loading: true,
-        error: false
+        error: false,
+        errorImg: false
     }
 
     marvelService = new MarvelService();
@@ -70,7 +71,7 @@ class RandomChar extends Component {
                     <p className="randomchar__title">
                         Or choose another one
                     </p>
-                    <button className="button button__main">
+                    <button onClick={this.updateChar} className="button button__main">
                         <div className="inner">try it</div>
                     </button>
                     <img src={mjolnir} alt="mjolnir" className="randomchar__decoration"/>
@@ -83,9 +84,15 @@ class RandomChar extends Component {
 const View = ({char}) => {
     const {name, description, thumbnail, homepage, wiki} = char;
 
+    let imgStyle = {'objectFit' : 'cover'};
+
+    if (thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
+        imgStyle = {'objectFit' : 'contain'}
+    }
+
     return (
         <div className="randomchar__block">
-        <img src={thumbnail} alt="Random character" className="randomchar__img"/>
+        <img src={thumbnail} style={imgStyle} alt="Random character" className="randomchar__img"/>
         <div className="randomchar__info">
             <p className="randomchar__name">{name}</p>
             <p className="randomchar__descr">
