ede76f3cbb44774fcce91717eec704c1747a15eb
Dmytro Shaposhnikov <dimacr137@gmail.com>
2024-12-22 14:32:54 +0200

    update CharLIst

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -4,6 +4,8 @@ class MarvelService {
     _apiBase = 'https://gateway.marvel.com:443/v1/public/';
     _apiKey = 'apikey=9d51ce1cddd9a93408d151a7a0c343a2';
 
+    _baseOffset = 210;
+
     getResource = async (url) => {
         let res = await fetch(url);
     
@@ -12,10 +14,10 @@ class MarvelService {
         }
     
         return await res.json();
-    };
+    }
 
-    getAllCharacters = async () => {
-        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    getAllCharacters = async (offset = this._baseOffset) => {
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`);
         return res.data.results.map(this._transformCharacter);
     }
 
@@ -28,7 +30,7 @@ class MarvelService {
         return {
             id: char.id,
             name: char.name,
-            description: char.description,
+            description: char.description ? `${char.description.slice(0, 210)}...` : 'There is no description for this character',
             thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
             homepage: char.urls[0].url,
             wiki: char.urls[1].url,
